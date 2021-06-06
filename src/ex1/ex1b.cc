2d79f8a7684e585416766959eee20494e2c27a76
AleCompaGSSI <alessandro.compagnucci@gssi.it>
2021-06-06 18:40:38 +0200

    Added mt19937, commented, to ex1b

--- src/ex1/ex1b.cc
+++ src/ex1/ex1b.cc
@@ -13,13 +13,19 @@ int main(){
 
     //for (size_t i = 0; i<10; i++) std::cout << random_double(n) << " " << std::endl ;
 
+    //std::random_device rd;
+    //std::mt19937 mt(rd());
+
+    //std::uniform_real_distribution<double> distr(0.0,1.0);
+
     TH1D *h1 = new TH1D("h1", "", 100, 0, 1 );
 
     std::cout << "Filling histogram ... " << std::endl;
 
-    for (size_t i = 0; i < 4294967295 ; i++) h1->Fill( random_double(n) );
+    for (size_t i = 0; i < 1000000 ; i++) h1->Fill( random_double(n) );
+    //for (size_t i = 0; i < 1000000 ; i++) h1->Fill( distr(mt) );
 
-    TF1 *f1 = new TF1(""," (4294967295.0 / 100.0) ", 0, 1 );
+    TF1 *f1 = new TF1(""," (1000000.0 / 100.0) ", 0, 1 );
 
 	std::cout << "Chisquare is : " << h1->Chisquare(f1) << std::endl;
 
