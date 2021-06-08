882c5880a2295df152575d3ffc177c40289fd166
AleCompaGSSI <alessandro.compagnucci@gssi.it>
2021-06-08 18:06:21 +0200

    Updated ex6c using templates

--- src/ex6/ex6.cc
+++ src/ex6/ex6.cc
@@ -3,30 +3,18 @@
 #include <random>
 #include <iostream>
 
-void sum_float(int N){
-    float x = 0;
-    for ( int i = 0; i < N; i++) x += 1.0 / (float) N ;
-    std::cout <<"Float\t:" << " " <<std::setprecision(10)<< x - 1.0 << "\t" << 1.0 / (float) N << " " << N << std::endl;
-  
-}
+template <typename T>
+void sum( int N ){
+    T x = 0;
+    for ( int i = 0; i < N; i++) x += 1.0 / static_cast<T>( N );
+    std::cout <<"\t" << " " <<std::setprecision(12)<< x - 1.0 << "\t" << 1.0 / static_cast<T>( N )<< " " << N << std::endl;  
 
-void sum_double(int N){
-    double x = 0;
-    for ( int i = 0; i < N; i++) x += 1.0 / (double) N ;
-    std::cout <<"Double\t:" << " " <<std::setprecision(12)<< x - 1.0 << "\t" << 1.0 / (double) N << " " << N << std::endl;  
 }
 
 
 int main(){
 
-
-    sum_float(10000);
-    sum_float(100000);
-    sum_float(1000000);
-
-    sum_double(10000);
-    sum_double(100000);
-    sum_double(1000000);
-
+    std::cout << "Float : \n" ; sum<float >(10000); sum<float >(100000); sum<float >(1000000);
+    std::cout << "Double : \n"; sum<double>(10000); sum<double>(100000); sum<double>(1000000);
 
 }
